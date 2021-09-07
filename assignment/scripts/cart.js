7963e281b38b8d66e4caba2d1764ad0b29de1396
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-09-07 14:51:19 -0500

    adds addItem()

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,22 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+const basket = [];
+
+// Create a function called addItem. It should:
+//
+// take an input parameter for a string item
+// add the new item to the global array basket.
+// return true indicating the item was added
+function addItem(item) {
+  basket.push(item);
+  return true;
+}
+
+// test
+console.log(`Basket is ${basket}`);
+console.log(`Adding apples (expect true)`, addItem('apples'));
+console.log(`Basket is now ${basket}`);
+console.log(`Basket is ${basket}`);
+console.log(`Adding pears (expect true)`, addItem('pears'));
+console.log(`Basket is now ${basket}`);
